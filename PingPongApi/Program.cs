5128dd1fde429392171e48ae7ae1511a48bc761f
hungdoan1031 <dth1vn@gmail.com>
2019-12-06 23:00:28 -0500

    iis integrated

--- PingPongApi/Program.cs
+++ PingPongApi/Program.cs
@@ -20,7 +20,9 @@ namespace PingPongAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .UseIISIntegration();
                 });
     }
 }
