7481cf143b1b785feaf4408d2bf16822276cbc6c
hungdoan1031 <dth1vn@gmail.com>
2019-12-07 15:33:05 -0500

    Cleaned up code

--- PingPongApi/Startup.cs
+++ PingPongApi/Startup.cs
@@ -26,7 +26,6 @@ namespace PingPongAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,9 +45,6 @@ namespace PingPongAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
